931568d7b8be8f710952e818b37060fdeea46c39
ggranum-fsltd <geoff.granum@gmail.com>
2020-01-15 15:12:56 -0800

    core: It would be nice to have a toMongoId utility method

--- core/src/test/java/com/geoffgranum/uttu/core/persistence/id/MongoStyleIdGeneratorTest.java
+++ core/src/test/java/com/geoffgranum/uttu/core/persistence/id/MongoStyleIdGeneratorTest.java
@@ -6,10 +6,14 @@
 
 package com.geoffgranum.uttu.core.persistence.id;
 
+import com.google.common.primitives.Ints;
 import org.testng.annotations.Test;
 
+import java.math.BigInteger;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
 
 /**
  * @author ggranum
@@ -20,5 +24,17 @@ public class MongoStyleIdGeneratorTest {
   public void testSearchForMacReturnsAnAddress() throws Exception {
     byte[] x = MongoStyleIdGenerator.searchNetworksForMacAddress();
     assertThat(x.length, is(6));
+  }
+
+
+  @Test
+  public void testCanConvertToMongoStyleByteArray() throws Exception {
+    long now = System.currentTimeMillis();
+
+    BigInteger id = new MongoStyleIdGenerator().nextId();
+    byte[] mongoId = MongoStyleIdGenerator.toMongoDb(id);
+    assertThat(mongoId.length, is(12));
+    int seconds = Ints.fromByteArray(mongoId);
+    assertThat("Time difference should be 1 second or less.", (long) (seconds) * 1000L - now, lessThan(2L));
   }
 }
