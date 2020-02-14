cd47cae8bcd60a4d895fbb4dd0c943007f9959e8
ggranum-fsltd <geoff.granum@gmail.com>
2020-02-14 10:29:34 -0800

    build: Set compile version to 12 for all subprojects.

--- mongodb/src/main/java/com/geoffgranum/uttu/mongodb/BuilderGenCodecProvider.java
+++ mongodb/src/main/java/com/geoffgranum/uttu/mongodb/BuilderGenCodecProvider.java
@@ -1,18 +1,14 @@
 package com.geoffgranum.uttu.mongodb;
 
 import com.geoffgranum.uttu.core.base.Buildable;
-import com.geoffgranum.uttu.core.exception.FatalException;
 import org.bson.codecs.Codec;
 import org.bson.codecs.configuration.CodecProvider;
 import org.bson.codecs.configuration.CodecRegistry;
 
 import javax.annotation.concurrent.Immutable;
 import javax.annotation.concurrent.ThreadSafe;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 /**
- *
  * @author ggranum
  */
 @Immutable
@@ -20,17 +16,14 @@ import java.lang.reflect.InvocationTargetException;
 public final class BuilderGenCodecProvider<T extends Buildable> implements CodecProvider {
 
   private final Class<T> buildableClass;
-  private final Class<BuilderGenCodec<T>> codecClass;
   private final ToBuildableTransform<T> fromDocumentFn;
   private final ToDocumentTransform<T> toDocumentFn;
 
   public BuilderGenCodecProvider(Class<T> buildableClass,
-                                 Class<BuilderGenCodec<T>> codecClass,
                                  ToBuildableTransform<T> fromDocumentFn,
                                  ToDocumentTransform<T> toDocumentFn
   ) {
     this.buildableClass = buildableClass;
-    this.codecClass = codecClass;
     this.fromDocumentFn = fromDocumentFn;
     this.toDocumentFn = toDocumentFn;
   }
@@ -39,10 +32,11 @@ public final class BuilderGenCodecProvider<T extends Buildable> implements Codec
    * Sadly the MongoDB Java API developers haven't yet picked up on the fact that nulls are bad. This method is used
    * to look up Codecs for your Buildable classes. If the class provided as 'clazz' does not match the class
    * registered for this instance of BuilderGenCodecProvider then this method will return null.
-   * @param clazz Can be any class that is retrieved or put into from a MongoDB MongoCollection via the typed
-   *              getter/setter methods.
+   *
+   * @param clazz    Can be any class that is retrieved or put into from a MongoDB MongoCollection via the typed
+   *                 getter/setter methods.
    * @param registry Provided by MongoDB invoker
-   * @param <X> The actual class type.
+   * @param <X>      The actual class type.
    * @return A BuilderGenCodecProvider<T extends Buildable> class, if one is found that matches clazz, otherwise null.
    */
   @SuppressWarnings("unchecked")
@@ -50,15 +44,7 @@ public final class BuilderGenCodecProvider<T extends Buildable> implements Codec
   public <X> Codec<X> get(Class<X> clazz, CodecRegistry registry) {
     Codec<X> result = null;
     if (clazz == buildableClass) {
-      try {
-        Constructor<BuilderGenCodec<T>> ctor = codecClass.getConstructor(CodecRegistry.class,
-          Class.class,
-          ToBuildableTransform.class,
-          ToDocumentTransform.class);
-        result = (Codec<X>) ctor.newInstance(registry, buildableClass, fromDocumentFn, toDocumentFn);
-      } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
-        throw new FatalException(e, "Could not construct Codec. ");
-      }
+      result = (Codec<X>) new BuilderGenCodec<T>(registry, buildableClass, fromDocumentFn, toDocumentFn);
     }
     return result;
   }
