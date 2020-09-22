d317a45cf74baecf9675df051e90974bda855c9f
ggranum <geoff.granum@daimler.com>
2020-09-22 09:56:24 -0700

    fix(client): Client filter shouldn't capture if debug not enabled

--- restclient/src/main/java/com/geoffgranum/uttu/restclient/CapturingClientResponseFilter.java
+++ restclient/src/main/java/com/geoffgranum/uttu/restclient/CapturingClientResponseFilter.java
@@ -6,49 +6,38 @@
 package com.geoffgranum.uttu.restclient;
 
 import com.geoffgranum.uttu.core.log.Log;
+import org.apache.commons.io.IOUtils;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientResponseContext;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import javax.ws.rs.client.ClientRequestContext;
-import javax.ws.rs.client.ClientResponseContext;
-import org.apache.commons.io.IOUtils;
 
 /**
  * @author Geoff M. Granum
  */
 public class CapturingClientResponseFilter extends LoggingClientResponseFilter {
 
-  private String responseBody;
-  private ClientRequestContext requestContext;
-  private ClientResponseContext responseContext;
-
-  public String getResponseBody() {
-    return responseBody;
-  }
-
-  public ClientRequestContext getRequestContext() {
-    return requestContext;
-  }
-
-  public ClientResponseContext getResponseContext() {
-    return responseContext;
-  }
-
-  @Override
-  public void filter(
-      ClientRequestContext requestContext,
-      ClientResponseContext responseContext) throws IOException {
-    this.requestContext = requestContext;
-    this.responseContext = responseContext;
-    InputStream stream = responseContext.getEntityStream();
-    // Stream can be null if response is empty.
-    if(stream != null) {
-      byte[] bytes = IOUtils.toByteArray(stream);
-      responseBody = new String(bytes, StandardCharsets.UTF_8);
-      responseContext.setEntityStream(new ByteArrayInputStream(bytes));
-      Log.debug(getClass(), responseBody);
+    @Override
+    public void filter(
+            ClientRequestContext requestContext,
+            ClientResponseContext responseContext) throws IOException {
+        if (Log.debugEnabled(getClass())) {
+            if (responseContext.getLength() > 10E3) {
+                Log.debug(getClass(), "Not logging response: contentLength of '%s' exceeds 10Kb", responseContext.getLength());
+            } else {
+                InputStream stream = responseContext.getEntityStream();
+                // Stream can be null if response is empty.
+                if (stream != null) {
+                    byte[] bytes = IOUtils.toByteArray(stream);
+                    String responseBody = new String(bytes, StandardCharsets.UTF_8);
+                    responseContext.setEntityStream(new ByteArrayInputStream(bytes));
+                    Log.debug(getClass(), responseBody);
+                }
+            }
+        }
     }
-  }
 }
  
